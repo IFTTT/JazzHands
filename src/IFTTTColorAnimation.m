717144eda6731c45994765596609c81a0ec898fb
Sean McMains <sean@mcmains.net>
2013-11-13 13:15:51 -0600

    Add support for grey colorspace in color transitions.

--- src/IFTTTColorAnimation.m
+++ src/IFTTTColorAnimation.m
@@ -26,8 +26,8 @@
     
     CGFloat startRed, startBlue, startGreen, startAlpha;
     CGFloat endRed, endBlue, endGreen, endAlpha;
-    if ([startKeyFrame.color getRed:&startRed green:&startGreen blue:&startBlue alpha:&startAlpha] &&
-        [endKeyFrame.color getRed:&endRed green:&endGreen blue:&endBlue alpha:&endAlpha]) {
+    if ([self getRed:&startRed green:&startGreen blue:&startBlue alpha:&startAlpha fromColor:startKeyFrame.color] &&
+        [self getRed:&endRed green:&endGreen blue:&endBlue alpha:&endAlpha fromColor:endKeyFrame.color]) {
         CGFloat red = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startRed endValue:endRed atTime:time];
         CGFloat green = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startGreen endValue:endGreen atTime:time];
         CGFloat blue = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startBlue endValue:endBlue atTime:time];
@@ -35,6 +35,19 @@
         animationFrame.color = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
     }
     return animationFrame;
+}
+
+- (BOOL)getRed:(CGFloat*)red green:(CGFloat*)green blue:(CGFloat*)blue alpha:(CGFloat*)alpha fromColor:(UIColor *)color {
+    CGFloat white;
+    if ([color getRed:red green:green blue:blue alpha:alpha]) {
+        return YES;
+    } else if ([color getWhite:&white alpha:alpha]) {
+        *red = white;
+        *green = white;
+        *blue = white;
+        return YES;
+    }
+    return NO;
 }
 
 @end
