ee3c78b2494a328b1cae65391ffec09f7d3ea4cd
Jonathan Hersh <jonathan@ifttt.com>
2014-03-17 10:01:40 -0700

    minor color animation cleanup

--- src/IFTTTColorAnimation.m
+++ src/IFTTTColorAnimation.m
@@ -8,6 +8,12 @@
 
 #import "IFTTTColorAnimation.h"
 
+@interface IFTTTColorAnimation ()
+
+- (BOOL) iftttGetRed:(CGFloat*)red green:(CGFloat*)green blue:(CGFloat*)blue alpha:(CGFloat*)alpha fromColor:(UIColor *)color;
+
+@end
+
 @implementation IFTTTColorAnimation
 
 - (void)animate:(NSInteger)time
@@ -26,19 +32,22 @@
     
     CGFloat startRed, startBlue, startGreen, startAlpha;
     CGFloat endRed, endBlue, endGreen, endAlpha;
-    if ([self getRed:&startRed green:&startGreen blue:&startBlue alpha:&startAlpha fromColor:startKeyFrame.color] &&
-        [self getRed:&endRed green:&endGreen blue:&endBlue alpha:&endAlpha fromColor:endKeyFrame.color]) {
+    
+    if ([self iftttGetRed:&startRed green:&startGreen blue:&startBlue alpha:&startAlpha fromColor:startKeyFrame.color] &&
+        [self iftttGetRed:&endRed green:&endGreen blue:&endBlue alpha:&endAlpha fromColor:endKeyFrame.color]) {
         CGFloat red = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startRed endValue:endRed atTime:time];
         CGFloat green = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startGreen endValue:endGreen atTime:time];
         CGFloat blue = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startBlue endValue:endBlue atTime:time];
         CGFloat alpha = [self tweenValueForStartTime:startKeyFrame.time endTime:endKeyFrame.time startValue:startAlpha endValue:endAlpha atTime:time];
         animationFrame.color = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
     }
+    
     return animationFrame;
 }
 
-- (BOOL)getRed:(CGFloat*)red green:(CGFloat*)green blue:(CGFloat*)blue alpha:(CGFloat*)alpha fromColor:(UIColor *)color {
+- (BOOL) iftttGetRed:(CGFloat *)red green:(CGFloat *)green blue:(CGFloat *)blue alpha:(CGFloat *)alpha fromColor:(UIColor *)color {
     CGFloat white;
+    
     if ([color getRed:red green:green blue:blue alpha:alpha]) {
         return YES;
     } else if ([color getWhite:&white alpha:alpha]) {
@@ -47,6 +56,7 @@
         *blue = white;
         return YES;
     }
+    
     return NO;
 }
 
