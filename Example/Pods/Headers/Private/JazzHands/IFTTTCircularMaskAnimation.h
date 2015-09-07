82cbdab8f8349047910d467bdc0c8aad1a95aa66
Jason Millard <jsm174@gmail.com>
2015-09-07 16:39:01 -0400

    Implemented circular mask animation

---
+++ Example/Pods/Headers/Private/JazzHands/IFTTTCircularMaskAnimation.h
@@ -0,0 +1,1 @@
+../../../../../JazzHands/IFTTTCircularMaskAnimation.h
---
+++ JazzHands/IFTTTCircularMaskAnimation.h
@@ -0,0 +1,16 @@
+//
+//  IFTTTCircularMaskAnimation.m
+//  JazzHands
+//
+//  Created by Jason Millard on 9/7/15.
+//  Copyright (c) 2015 IFTTT Inc. All rights reserved.
+//
+
+#import "IFTTTViewAnimation.h"
+
+@interface IFTTTCircularMaskAnimation : IFTTTViewAnimation <IFTTTAnimatable>
+
+- (void)addKeyframeForTime:(CGFloat)time visibility:(CGFloat)percent;
+- (void)addKeyframeForTime:(CGFloat)time visibility:(CGFloat)percent withEasingFunction:(IFTTTEasingFunction)easingFunction;
+
+@end
